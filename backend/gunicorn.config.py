ff9fd3501d04fca9c307b03bece6559bbc317e67
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-10-31 17:05:17 +0530

    added condition for reload in gunicorn

--- backend/gunicorn.config.py
+++ backend/gunicorn.config.py
@@ -5,7 +5,9 @@ from settings.__init__ import __version__
 load_dotenv(".env")
 bind = f"{os.getenv('BASE_HOST')}:{os.getenv('BASE_PORT')}"
 worker_class = "uvicorn.workers.UvicornWorker"
-# max_requests = 1
+# After every request the server will restart only use this in debug mode
+if eval(os.getenv("DEBUG")):
+    max_requests = 1
 
 
 def on_starting(server):
