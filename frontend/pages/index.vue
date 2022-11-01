cd661830a91952d86c4b5731e49e334ea9cc8764
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-11-01 18:48:24 +0530

    added filtering part and testing section

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <Tutorial/>
+	<div>
+		<section class="section">
+			<div class="box">
+				<FilterBasic />
+			</div>
+		</section>
+
+		<section class="section" v-for="item in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]" :key="item">
+			<div class="box">
+				<div class="field">
+					<label class="label">Email</label>
+					<div class="control">
+						<input class="input" type="email" placeholder="e.g. alex@example.com" />
+					</div>
+				</div>
+
+				<div class="field">
+					<label class="label">Password</label>
+					<div class="control">
+						<input class="input" type="password" placeholder="********" />
+					</div>
+				</div>
+
+				<button class="button is-info">Sign in</button>
+				<b-tag rounded icon="account-check-outline">Rounded Tag</b-tag>
+			</div>
+		</section>
+	</div>
 </template>
 
 <script>
