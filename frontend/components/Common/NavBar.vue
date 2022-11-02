eef60fd53e0df7227e231f03c0279488eb105c4a
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-11-02 12:40:10 +0530

    added navbar component

---
+++ frontend/components/Common/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+	<b-navbar shadow fixed-top>
+		<template #brand>
+			<b-navbar-item tag="router-link" :to="{ path: '/' }">
+				<span class="is-size-5 has-text-weight-bold">XPLORECoV2</span>
+			</b-navbar-item>
+		</template>
+
+		<template #end>
+			<b-navbar-item tag="div">
+				<div class="buttons">
+					<a class="button is-light"> Log in </a>
+				</div>
+			</b-navbar-item>
+		</template>
+	</b-navbar>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style scoped></style>
