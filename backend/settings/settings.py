3f3df972d5262b57f15e236fc72a8f02c7ba8b76
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-11-02 12:36:13 +0530

    updated database path

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -82,7 +82,7 @@ ASGI_APPLICATION = "settings.asgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "database" / "db.sqlite3",
     }
 }
 
