81e9021c5bde3a85dd01d10d6c2729bbe691f7f5
BhupathiSampath <samastha849@gmail.com>
2022-11-02 15:02:37 +0530

    added api to get distinct deletions autocomplete

---
+++ backend/queryhub/api/autocomplete/deletion.py
@@ -0,0 +1,32 @@
+import operator
+import datetime
+from functools import reduce
+from django.db.models import Q
+from queryhub.models import QueryHubModel
+from datetime import date, timedelta
+from rest_framework.response import Response
+from rest_framework import generics, exceptions, serializers, status
+
+
+class DeletionAutoCompleteSerializer(serializers.Serializer):
+    def validate(self, value):
+        obj = list(QueryHubModel.objects.values_list("aadeletions", flat=True))
+        data = []
+        for i in obj:
+            if not i is None:
+                data.extend(i.split(","))
+        data.append("None")
+        return sorted(list(set(data)))
+
+
+class DeletionAutoCompleteView(generics.GenericAPIView):
+    serializer_class = DeletionAutoCompleteSerializer
+
+    def post(self, request, *args, **kwargs):
+        self.serializer = self.get_serializer(data=request.data)
+        if self.serializer.is_valid():
+            return Response(self.serializer.validated_data)
+        return Response(
+            create_uniform_response(self.serializer.errors),
+            status=status.HTTP_406_NOT_ACCEPTABLE,
+        )
