fa4aa625961a76dd5f131e705d0dfaae188148ea
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-11-06 19:01:40 +0530

    added states for table and pages

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,7 +1,61 @@
-export const state = () => ({})
+import { ToastProgrammatic as Toast } from 'buefy'
+import { getField, updateField } from 'vuex-map-fields'
 
-export const mutations = {}
+export const state = () => ({
+	total_pages: 1,
+	table_data: [],
+	filters: {
+		page: 1,
+		clade: [],
+		state: [],
+		dates: [],
+		deletion: [],
+		pangolineage: [],
+		substitution: [],
+		nextcladelineage: [],
+	},
+})
+
+export const getters = {
+	getField,
+}
+
+export const mutations = {
+	updateField,
+	SET_TABLE(state, payload) {
+		state.table_data = payload
+	},
+	SET_PAGES(state, payload) {
+		state.total_pages = payload
+	},
+}
 
 export const actions = {
 	async nuxtServerInit({ commit, dispatch }, { app, store }) {},
+	async GetTable({ commit, dispatch }) {
+		try {
+			const response = await this.$axios.$post('/query/')
+			await commit('SET_TABLE', response.data)
+			await commit('SET_PAGES', response.length)
+		} catch (err) {
+			Toast.open({
+				message: 'err',
+				type: 'is-danger',
+				position: 'is-top-right',
+			})
+		}
+	},
+	async UpdateTable({ commit, dispatch, state }) {
+		try {
+			const response = await this.$axios.$post('/query/', state.filters)
+			await commit('SET_TABLE', response.data)
+			await commit('SET_PAGES', response.length)
+		} catch (err) {
+			Toast.open({
+				message: 'err',
+				type: 'is-danger',
+				position: 'is-top-right',
+			})
+		}
+	},
 }
