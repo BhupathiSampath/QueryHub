2832f2a0abe800c08854f149d9a040fc0382dbd6
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-11-06 22:27:12 +0530

    removed unnecessary imports and converted serializer to normal serializer and updated input parameters

--- backend/queryhub/api/state_wise_sequences.py
+++ backend/queryhub/api/state_wise_sequences.py
@@ -1,43 +1,26 @@
-import operator
-import datetime
-from functools import reduce
-from django.db.models import Q
 from django.db.models import Count
 from ..models import QueryHubModel
-from datetime import date, timedelta
 from .utils import create_uniform_response
 from rest_framework.response import Response
 from .tasks import text_search, advenced_filter
 from rest_framework import generics, exceptions, serializers, status
 
 
-class StateSequencesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = QueryHubModel
-        fields = (
-            "date",
-            "clade",
-            "strain",
-            "lineage",
-            "division",
-            "aadeletions",
-            "aasubstitutions",
-            "nextclade_pango",
-        )
-
+class StateSequencesSerializer(serializers.Serializer):
     def validate(self, value):
-        date = value.get("date")
-        clade = value.get("clade")
-        strain = value.get("strain")
-        lineage = value.get("lineage")
-        division = value.get("division")
-        aadeletions = value.get("aadeletions")
-        nextclade_pango = value.get("nextclade_pango")
-        aasubstitutions = value.get("aasubstitutions")
-        params = self.context.get("request").data
-        days = params.get("days")
-        search = params.get("search")
-        present = params.get("present")
+        request = self.context.get("request").data
+        date = request.get("date")
+        days = request.get("days")
+        clade = request.get("clade")
+        page = request.get("page", 1)
+        search = request.get("search")
+        strain = request.get("strain")
+        present = request.get("present")
+        division = request.get("state")
+        lineage = request.get("pangolineage")
+        aadeletions = request.get("deletion")
+        nextclade_pango = request.get("nextcladelineage")
+        aasubstitutions = request.get("substitution")
         obj = QueryHubModel.objects
         if search:
             obj = text_search(search, obj)
