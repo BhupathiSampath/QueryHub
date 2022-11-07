209cc56bf69d0164858f4002b76f2d500c628960
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-11-07 18:48:46 +0530

    renaming the state label for MapGraph

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -120,6 +120,50 @@ export default {
 			'graphs.state_graph_loaded',
 			'graphs.seq_week_graph_loaded',
 		]),
+		RenamedStateLabel() {
+			let rename = {
+				AP: 'Andhra Pradesh',
+				AR: 'Arunachal Pradesh',
+				AS: 'Assam',
+				BR: 'Bihar',
+				CT: 'Chhattisgarh',
+				GA: 'Goa',
+				GJ: 'Gujarat',
+				HR: 'Haryana',
+				HP: 'Himachal Pradesh',
+				JH: 'Jharkhand',
+				KA: 'Karnataka',
+				KL: 'Kerala',
+				MP: 'Madhya Pradesh',
+				MH: 'Maharashtra',
+				MN: 'Manipur',
+				ML: 'Meghalaya',
+				MZ: 'Mizoram',
+				NL: 'Nagaland',
+				OR: 'Odisha',
+				PB: 'Punjab',
+				RJ: 'Rajasthan',
+				SK: 'Sikkim',
+				TN: 'Tamil Nadu',
+				TG: 'Telangana',
+				TR: 'Tripura',
+				UT: 'Uttarakhand',
+				UP: 'Uttar Pradesh',
+				WB: 'West Bengal',
+				AN: 'Andaman and Nicobar Islands',
+				CH: 'Chandigarh',
+				DN: 'Dadra and Nagar Haveli and Daman and Diu',
+				DL: 'Delhi',
+				JK: 'Jammu and Kashmir',
+				LA: 'Ladakh',
+				LD: 'Lakshadweep',
+				PY: 'Puducherry',
+			}
+			let output = map(this.state, (d) => {
+				return { name: rename[d.label], value: d.value }
+			})
+			return output
+		},
 	},
 	methods: {
 		StateFormatter(params) {
