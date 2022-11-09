1d76060db5e54a0d149bb032a87bd8342a50f9e4
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-11-09 17:12:14 +0530

    Testing footer

--- frontend/components/Common/Footer.vue
+++ frontend/components/Common/Footer.vue
@@ -1,7 +1,11 @@
 <template>
-	<b-navbar>
+	<b-navbar :mobile-burger="false" centered class="footer has-background-grey-lighter" shadow>
 		<template #start>
-			<div class="box has-background-info-light">Footer add here</div>
+			<footer>
+				<div class="content has-text-centered">
+					<p>Magna cupidatat sit labore dolore occaecat id in esse eu ad reprehenderit quis dolor.</p>
+				</div>
+			</footer>
 		</template>
 	</b-navbar>
 </template>
