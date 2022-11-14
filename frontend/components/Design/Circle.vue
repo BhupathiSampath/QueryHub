5b45970978f6a4b98585c19d4dfe70756aa99d9e
Animesh Kumar Singh <animesh.workplace@gmail.com>
2022-11-14 19:44:54 +0530

    linter

--- frontend/components/Design/Circle.vue
+++ frontend/components/Design/Circle.vue
@@ -1,15 +1,15 @@
 <template>
 	<svg
-		class="spinner Spinner-sc-10nq2ph-0 faqfqd"
+		width="640"
+		height="640"
+		version="1.1"
+		viewBox="0 0 640 640"
 		xmlns="http://www.w3.org/2000/svg"
 		xmlns:xlink="http://www.w3.org/1999/xlink"
-		width="638"
-		height="638"
-		viewBox="0 0 638 638"
-		version="1.1"
+		class="spinner Spinner-sc-10nq2ph-0 faqfqd"
 	>
 		<defs>
-			<rect id="path-1" x="0" y="0" width="638" height="638"></rect>
+			<rect id="path-1" x="0" y="0" width="640" height="640"></rect>
 			<rect id="path-3" x="0" y="0" width="376" height="376"></rect>
 		</defs>
 		<g id="Designs" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -24,22 +24,22 @@
 							transform="translate(90.500000, 254.000000) scale(-1, 1) rotate(38.000000) translate(-90.500000, -254.000000) translate(-66.000000, 126.000000)"
 						>
 							<path
-								d="M16.31 15.86C16.31 15.86 251.96 10.5 312.31 255.86"
-								id="Path-16"
-								stroke="#36CFFF"
-								stroke-width="30"
 								fill="none"
+								id="Path-16"
+								stroke="#087FD2"
+								stroke-width="30"
 								stroke-linecap="round"
+								d="M16.31 15.86C16.31 15.86 251.96 10.5 312.31 255.86"
 							></path>
 							<circle
-								id="Oval-7"
-								stroke="none"
-								fill-opacity="0.16"
-								fill="#000000"
-								fill-rule="evenodd"
+								r="15"
 								cx="15.31"
 								cy="15.86"
-								r="15"
+								id="Oval-7"
+								stroke="none"
+								fill="#000000"
+								fill-opacity="0.16"
+								fill-rule="evenodd"
 							></circle>
 						</g>
 					</g>
@@ -48,22 +48,22 @@
 							transform="translate(388.171879, 91.171879) scale(-1, 1) rotate(-53.000000) translate(-388.171879, -91.171879) translate(231.671879, -36.828121)"
 						>
 							<path
-								d="M16.31 15.86C16.31 15.86 251.96 10.5 312.31 255.86"
-								id="Path-16"
-								stroke="#612DF7"
-								stroke-width="30"
 								fill="none"
+								id="Path-16"
+								stroke="#52A5E0"
+								stroke-width="30"
 								stroke-linecap="round"
+								d="M16.31 15.86C16.31 15.86 251.96 10.5 312.31 255.86"
 							></path>
 							<circle
-								id="Oval-7"
-								stroke="none"
-								fill-opacity="0.16"
-								fill="#000000"
-								fill-rule="evenodd"
+								r="15"
 								cx="15.31"
 								cy="15.86"
-								r="15"
+								id="Oval-7"
+								stroke="none"
+								fill="#000000"
+								fill-opacity="0.16"
+								fill-rule="evenodd"
 							></circle>
 						</g>
 					</g>
@@ -72,22 +72,22 @@
 							transform="translate(547.532976, 388.101252) scale(-1, 1) rotate(-143.000000) translate(-547.532976, -388.101252) translate(391.032976, 260.101252)"
 						>
 							<path
-								d="M16.31 15.86C16.31 15.86 251.96 10.5 312.31 255.86"
-								id="Path-16"
-								stroke="#45D9BE"
-								stroke-width="30"
 								fill="none"
+								id="Path-16"
+								stroke="#CEE5F6"
+								stroke-width="30"
 								stroke-linecap="round"
+								d="M16.31 15.86C16.31 15.86 251.96 10.5 312.31 255.86"
 							></path>
 							<circle
-								id="Oval-7"
-								stroke="none"
-								fill-opacity="0.16"
-								fill="#000000"
-								fill-rule="evenodd"
+								r="15"
 								cx="15.31"
 								cy="15.86"
-								r="15"
+								id="Oval-7"
+								stroke="none"
+								fill="#000000"
+								fill-opacity="0.16"
+								fill-rule="evenodd"
 							></circle>
 						</g>
 					</g>
@@ -96,22 +96,22 @@
 							transform="translate(251.171879, 547.171879) scale(-1, 1) rotate(-233.000000) translate(-251.171879, -547.171879) translate(94.671879, 419.171879)"
 						>
 							<path
-								d="M16.31 15.86C16.31 15.86 251.96 10.5 312.31 255.86"
-								id="Path-16"
-								stroke="#48484C"
-								stroke-width="30"
 								fill="none"
+								id="Path-16"
+								stroke="#065F9E"
+								stroke-width="30"
 								stroke-linecap="round"
+								d="M16.31 15.86C16.31 15.86 251.96 10.5 312.31 255.86"
 							></path>
 							<circle
-								id="Oval-7"
-								stroke="none"
-								fill-opacity="0.16"
-								fill="#000000"
-								fill-rule="evenodd"
+								r="15"
 								cx="15.31"
 								cy="15.86"
-								r="15"
+								id="Oval-7"
+								stroke="none"
+								fill="#000000"
+								fill-opacity="0.16"
+								fill-rule="evenodd"
 							></circle>
 						</g>
 					</g>
@@ -120,20 +120,20 @@
 					<g transform="translate(76.000000, 76.000000)">
 						<g id="Group-68" transform="translate(242.000000, 6.000000)">
 							<path
-								d="M227.91 172.12C227.91 172.12 186.55 6.34 0 0.71"
 								id="Path-13"
-								stroke="#48484C"
+								stroke="#CEE5F6"
 								stroke-width="30"
 								stroke-linecap="round"
 								stroke-linejoin="round"
+								d="M227.91 172.12C227.91 172.12 186.55 6.34 0 0.71"
 							></path>
 							<circle
-								id="Oval-6"
-								fill-opacity="0.16"
-								fill="#000000"
+								r="15"
 								cx="228"
 								cy="172"
-								r="15"
+								id="Oval-6"
+								fill="#000000"
+								fill-opacity="0.16"
 							></circle>
 						</g>
 						<g
@@ -141,20 +141,20 @@
 							transform="translate(386.500000, 364.500000) rotate(-270.000000) translate(-386.500000, -364.500000) translate(265.000000, 271.000000)"
 						>
 							<path
-								d="M227.91 172.12C227.91 172.12 186.55 6.34 0 0.71"
 								id="Path-13"
-								stroke="#612DF7"
+								stroke="#52A5E0"
 								stroke-width="30"
 								stroke-linecap="round"
 								stroke-linejoin="round"
+								d="M227.91 172.12C227.91 172.12 186.55 6.34 0 0.71"
 							></path>
 							<circle
-								id="Oval-6"
-								fill-opacity="0.16"
-								fill="#000000"
+								r="15"
 								cx="228"
 								cy="172"
-								r="15"
+								id="Oval-6"
+								fill="#000000"
+								fill-opacity="0.16"
 							></circle>
 						</g>
 						<g
@@ -162,20 +162,20 @@
 							transform="translate(121.500000, 386.500000) rotate(-180.000000) translate(-121.500000, -386.500000) translate(0.000000, 293.000000)"
 						>
 							<path
-								d="M227.91 172.12C227.91 172.12 186.55 6.34 0 0.71"
 								id="Path-13"
-								stroke="#36CFFF"
+								stroke="#087FD2"
 								stroke-width="30"
 								stroke-linecap="round"
 								stroke-linejoin="round"
+								d="M227.91 172.12C227.91 172.12 186.55 6.34 0 0.71"
 							></path>
 							<circle
-								id="Oval-6"
-								fill-opacity="0.16"
-								fill="#000000"
+								r="15"
 								cx="228"
 								cy="172"
-								r="15"
+								id="Oval-6"
+								fill="#000000"
+								fill-opacity="0.16"
 							></circle>
 						</g>
 						<g
@@ -183,20 +183,20 @@
 							transform="translate(99.500000, 121.500000) rotate(-90.000000) translate(-99.500000, -121.500000) translate(-22.000000, 28.000000)"
 						>
 							<path
-								d="M227.91 172.12C227.91 172.12 186.55 6.34 0 0.71"
 								id="Path-13"
-								stroke="#612DF7"
+								stroke="#065F9E"
 								stroke-width="30"
 								stroke-linecap="round"
 								stroke-linejoin="round"
+								d="M227.91 172.12C227.91 172.12 186.55 6.34 0 0.71"
 							></path>
 							<circle
-								id="Oval-6"
-								fill-opacity="0.16"
-								fill="#000000"
+								r="15"
 								cx="228"
 								cy="172"
-								r="15"
+								id="Oval-6"
+								fill="#000000"
+								fill-opacity="0.16"
 							></circle>
 						</g>
 					</g>
@@ -209,24 +209,24 @@
 								transform="translate(191.500000, 44.500000) rotate(-328.000000) translate(-191.500000, -44.500000) translate(102.000000, -19.000000)"
 							>
 								<path
-									d="M0.92 126.49C0.92 126.49 40.12 14.49 161.92 14.49"
 									id="Path-12"
-									stroke="#612DF7"
-									stroke-width="30"
 									fill="none"
+									stroke="#52A5E0"
+									stroke-width="30"
 									stroke-linecap="round"
 									stroke-linejoin="round"
+									d="M0.92 126.49C0.92 126.49 40.12 14.49 161.92 14.49"
 								></path>
 								<circle
-									id="Oval-2"
-									stroke="none"
-									fill-opacity="0.16"
-									fill="#000000"
-									fill-rule="evenodd"
-									transform="translate(163.301974, 15.224935) rotate(-360.000000) translate(-163.301974, -15.224935) "
+									r="15"
 									cx="163.3"
 									cy="15.22"
-									r="15"
+									id="Oval-2"
+									stroke="none"
+									fill="#000000"
+									fill-opacity="0.16"
+									fill-rule="evenodd"
+									transform="translate(163.301974, 15.224935) rotate(-360.000000) translate(-163.301974, -15.224935) "
 								></circle>
 							</g>
 						</g>
@@ -235,24 +235,24 @@
 								transform="translate(184.764960, 331.075976) rotate(-148.000000) translate(-184.764960, -331.075976) translate(96.264960, 267.075976)"
 							>
 								<path
-									d="M0.39 127.03C0.39 127.03 39.59 15.03 161.39 15.03"
-									id="Path-12"
-									stroke="#48484C"
-									stroke-width="30"
 									fill="none"
+									id="Path-12"
+									stroke="#065F9E"
+									stroke-width="30"
 									stroke-linecap="round"
 									stroke-linejoin="round"
+									d="M0.39 127.03C0.39 127.03 39.59 15.03 161.39 15.03"
 								></path>
 								<circle
+									r="15"
+									cy="15.12"
+									cx="161.83"
 									id="Oval-2"
 									stroke="none"
-									fill-opacity="0.16"
 									fill="#000000"
+									fill-opacity="0.16"
 									fill-rule="evenodd"
 									transform="translate(161.834814, 15.117082) rotate(-360.000000) translate(-161.834814, -15.117082) "
-									cx="161.83"
-									cy="15.12"
-									r="15"
 								></circle>
 							</g>
 						</g>
@@ -261,24 +261,24 @@
 								transform="translate(44.764960, 184.075976) rotate(-58.000000) translate(-44.764960, -184.075976) translate(-44.235040, 120.575976)"
 							>
 								<path
-									d="M0.02 126.43C0.02 126.43 39.22 14.43 161.02 14.43"
-									id="Path-12"
-									stroke="#48484C"
-									stroke-width="30"
 									fill="none"
+									id="Path-12"
+									stroke="#CEE5F6"
+									stroke-width="30"
 									stroke-linecap="round"
 									stroke-linejoin="round"
+									d="M0.02 126.43C0.02 126.43 39.22 14.43 161.02 14.43"
 								></path>
 								<circle
+									r="15"
+									cy="15.4"
+									cx="162.01"
 									id="Oval-2"
 									stroke="none"
-									fill-opacity="0.16"
 									fill="#000000"
+									fill-opacity="0.16"
 									fill-rule="evenodd"
 									transform="translate(162.014468, 15.404588) rotate(-360.000000) translate(-162.014468, -15.404588) "
-									cx="162.01"
-									cy="15.4"
-									r="15"
 								></circle>
 							</g>
 						</g>
@@ -287,24 +287,24 @@
 								transform="translate(331.227680, 193.919335) rotate(-237.000000) translate(-331.227680, -193.919335) translate(242.227680, 130.419335)"
 							>
 								<path
-									d="M0.13 126.63C0.13 126.63 39.32 14.63 161.13 14.63"
-									id="Path-12"
-									stroke="#36CFFF"
-									stroke-width="30"
 									fill="none"
+									id="Path-12"
+									stroke="#087FD2"
+									stroke-width="30"
 									stroke-linecap="round"
 									stroke-linejoin="round"
+									d="M0.13 126.63C0.13 126.63 39.32 14.63 161.13 14.63"
 								></path>
 								<circle
+									r="15"
+									cy="15.23"
+									cx="162.38"
 									id="Oval-2"
 									stroke="none"
-									fill-opacity="0.16"
 									fill="#000000"
+									fill-opacity="0.16"
 									fill-rule="evenodd"
 									transform="translate(162.376413, 15.226430) rotate(-360.000000) translate(-162.376413, -15.226430) "
-									cx="162.38"
-									cy="15.23"
-									r="15"
 								></circle>
 							</g>
 						</g>
